ff1efe878afdfd648f9b2cbc352c7abb77dfb57b
EncryptCrystal <52292872+EncryptCrystal@users.noreply.github.com>
2026-03-28 12:31:27 +0100

    .copy() -> copy.deepcopy()

--- Classes/state.py
+++ Classes/state.py
@@ -1,5 +1,6 @@
 from cube import Cube
-from ..Enumerations.action_type import ActionType
+import copy
+from Enumerations.action_type import ActionType
 
 class State:
     def __init__(self, cube: Cube, nbActions: int, parentState: State | None, actionType: ActionType):
@@ -12,51 +13,51 @@ class State:
     def expand(self) -> list[State]:
         children_states: list[State] = []
         
-        new_cube: Cube = self.cube.copy()
+        new_cube: Cube = copy.deepcopy(self.cube)
         new_cube.turnUp()
         children_states.append(State(new_cube, self.nbrActions + 1, self, ActionType.TURN_UP))
         
-        new_cube = self.cube.copy()
+        new_cube = copy.deepcopy(self.cube)
         new_cube.turnLeft()
         children_states.append(State(new_cube, self.nbrActions + 1, self, ActionType.TURN_LEFT))
         
-        new_cube = self.cube.copy()
+        new_cube = copy.deepcopy(self.cube)
         new_cube.turnFront()
         children_states.append(State(new_cube, self.nbrActions + 1, self, ActionType.TURN_FRONT))
         
-        new_cube = self.cube.copy()
+        new_cube = copy.deepcopy(self.cube)
         new_cube.turnRight()
         children_states.append(State(new_cube, self.nbrActions + 1, self, ActionType.TURN_RIGHT))
         
-        new_cube = self.cube.copy()
+        new_cube = copy.deepcopy(self.cube)
         new_cube.turnDown()
         children_states.append(State(new_cube, self.nbrActions + 1, self, ActionType.TURN_DOWN))
         
-        new_cube = self.cube.copy()
+        new_cube = copy.deepcopy(self.cube)
         new_cube.turnBack()
         children_states.append(State(new_cube, self.nbrActions + 1, self, ActionType.TURN_BACK))
         
-        new_cube = self.cube.copy()
+        new_cube = copy.deepcopy(self.cube)
         new_cube.returnUp()
         children_states.append(State(new_cube, self.nbrActions + 1, self, ActionType.RETURN_UP))
         
-        new_cube = self.cube.copy()
+        new_cube = copy.deepcopy(self.cube)
         new_cube.returnLeft()
         children_states.append(State(new_cube, self.nbrActions + 1, self, ActionType.RETURN_LEFT))
         
-        new_cube = self.cube.copy()
+        new_cube = copy.deepcopy(self.cube)
         new_cube.returnFront()
         children_states.append(State(new_cube, self.nbrActions + 1, self, ActionType.RETURN_FRONT))
         
-        new_cube = self.cube.copy()
+        new_cube = copy.deepcopy(self.cube)
         new_cube.returnRight()
         children_states.append(State(new_cube, self.nbrActions + 1, self, ActionType.RETURN_RIGHT))
         
-        new_cube = self.cube.copy()
+        new_cube = copy.deepcopy(self.cube)
         new_cube.returnDown()
         children_states.append(State(new_cube, self.nbrActions + 1, self, ActionType.RETURN_DOWN))
         
-        new_cube = self.cube.copy()
+        new_cube = copy.deepcopy(self.cube)
         new_cube.returnBack()
         children_states.append(State(new_cube, self.nbrActions + 1, self, ActionType.RETURN_BACK))
         
